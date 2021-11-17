a14bfac8d3980b820967bf9c986b6f17741679f6
rupali18082003 <missrupalithenaivegirl@gmail.com>
2021-11-17 11:33:04 +0530

    finishing

---
+++ script.js
@@ -0,0 +1,79 @@
+const input = document.querySelector('#input');
+const btn = document.querySelector('#shorten');
+const btnCopy = document.querySelector('#copy');
+const history = document.querySelector('#history'); 
+
+const shortIt = async function(link){
+	try{
+		const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}/very/long/link.html`);
+
+		const data = await res.json();
+		return data;
+	}catch(err){
+		throw new Error("Failed to fetch data. Please try again :(")
+	}
+}
+
+btn.addEventListener('click', function(){
+	const link = input.value;
+	if(!link){
+		alert("Please input a valid link :(");
+		return;
+	}
+
+	const data = shortIt(link);
+	data.then(res => {
+		function store(sl){
+			let links;
+			let ol;
+			if(localStorage.getItem('links') && localStorage.getItem('ol')){ 
+				links = JSON.parse(localStorage.getItem('links'))
+				ol = JSON.parse(localStorage.getItem('ol'))
+			}
+			else{
+				links = []
+				ol = [];
+			}
+
+			links.push(sl)
+			ol.push(link);
+
+			localStorage.setItem('links', JSON.stringify(links));
+			localStorage.setItem('ol', JSON.stringify(ol));
+
+
+			if(links){
+				history.innerHTML = '';
+				for(let i in links){
+					const markup = `	
+						<div class="p-3 lnk">
+					        <div class="original">${ol[i]}</div>
+					        <div class="shorted">${links[i]}</div>
+					        <button class="btn btn-danger rounded ms-md-4" id="copy" data-copy-to-clipboard ="${links[i]}">Copy</button>
+					     </div>
+						`;
+
+					history.insertAdjacentHTML('beforeend', markup);
+				}
+			}
+		}
+		store(res.result.short_link3)
+		input.value = '';
+
+	}).catch(err => alert(err.message))
+})
+
+const c2c =  function(){
+	history.addEventListener('click', function(e){
+		const copy = e.target.closest('#copy');
+
+		if(!copy) return;
+
+		const cpy = copy.dataset.copyToClipboard;
+		console.log(cpy);
+		navigator.clipboard.writeText(cpy);
+		copy.style.backgroundColor = "hsl(255, 11%, 22%)";
+		copy.textContent = "Copied!";
+	})
+}
+c2c();
