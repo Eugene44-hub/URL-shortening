2cac567a8e0df88a16d0a60be5466702e6931250
rupali18082003 <missrupalithenaivegirl@gmail.com>
2021-11-17 15:39:01 +0530

    updated

---
+++ app.js
@@ -0,0 +1,28 @@
+// removed for temporary time.
+
+const inputField = document.querySelector('#input-link')
+const inputGroup = document.querySelector('.input-parent')
+
+inputGroup.addEventListener('click', e => {
+    if (e.target.id === 'shorten') {
+        if (inputField.value === '') {
+            errorMessage()
+            setTimeout(() => {
+                clearError()
+            }, 3000)
+        }
+    }
+})
+
+
+function errorMessage() {
+    document.querySelector('#error').innerHTML = `
+    <div class='text-danger' id='message'>
+    please input something in the field
+    </div>
+    `
+}
+
+function clearError() {
+    document.querySelector('#error').style.display = 'none';
+}
--- script.js
+++ script.js
@@ -1,79 +1,93 @@
 const input = document.querySelector('#input');
+const error = document.querySelector('#err');
 const btn = document.querySelector('#shorten');
-const btnCopy = document.querySelector('#copy');
-const history = document.querySelector('#history'); 
+const list = document.querySelector('.list');
+const history = document.querySelector('#history');
 
-const shortIt = async function(link){
-	try{
-		const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}/very/long/link.html`);
+//fetching data
+const shortIt = async function(link) {
+    try {
+        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${link}/very/long/link.html`);
 
-		const data = await res.json();
-		return data;
-	}catch(err){
-		throw new Error("Failed to fetch data. Please try again :(")
-	}
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        throw new Error("Failed to load data. Please try again :(")
+    }
 }
 
-btn.addEventListener('click', function(){
-	const link = input.value;
-	if(!link){
-		alert("Please input a valid link :(");
-		return;
-	}
+//btn shorten it
+btn.addEventListener('click', function() {
+    const link = input.value;
+    try {
+        if (!link) {
+            throw new Error("Please input a valid link :(");
+        }
+        error.classList.add('hidden');
+        input.style.border = 'none';
+        const data = shortIt(link);
+        data.then(res => {
+            function store(sl) {
+                let links;
+                let ol;
+                if (localStorage.getItem('links') && localStorage.getItem('ol')) {
+                    links = JSON.parse(localStorage.getItem('links'))
+                    ol = JSON.parse(localStorage.getItem('ol'))
+                } else {
+                    links = []
+                    ol = [];
+                }
 
-	const data = shortIt(link);
-	data.then(res => {
-		function store(sl){
-			let links;
-			let ol;
-			if(localStorage.getItem('links') && localStorage.getItem('ol')){ 
-				links = JSON.parse(localStorage.getItem('links'))
-				ol = JSON.parse(localStorage.getItem('ol'))
-			}
-			else{
-				links = []
-				ol = [];
-			}
+                links.push(sl)
+                ol.push(link);
 
-			links.push(sl)
-			ol.push(link);
-
-			localStorage.setItem('links', JSON.stringify(links));
-			localStorage.setItem('ol', JSON.stringify(ol));
+                localStorage.setItem('links', JSON.stringify(links));
+                localStorage.setItem('ol', JSON.stringify(ol));
 
 
-			if(links){
-				history.innerHTML = '';
-				for(let i in links){
-					const markup = `	
-						<div class="p-3 lnk">
-					        <div class="original">${ol[i]}</div>
-					        <div class="shorted">${links[i]}</div>
-					        <button class="btn btn-danger rounded ms-md-4" id="copy" data-copy-to-clipboard ="${links[i]}">Copy</button>
-					     </div>
-						`;
+                if (links) {
+                    history.innerHTML = '';
+                    list.classList.remove('hidden');
+                    for (let i in links) {
+                        const markup = `    
+                        <div class="p-3 lnk">
+                            <div class="original">${ol[i]}</div>
+                            <div class="shorted">${links[i]}</div>
+                            <button class="btn btn-danger rounded ms-md-4" id="copy" data-copy-to-clipboard ="${links[i]}">Copy</button>
+                         </div>
+                        `;
 
-					history.insertAdjacentHTML('beforeend', markup);
-				}
-			}
-		}
-		store(res.result.short_link3)
-		input.value = '';
-
-	}).catch(err => alert(err.message))
+                        history.insertAdjacentHTML('afterbegin', markup);
+                    }
+                }
+            }
+            store(res.result.short_link3)
+            input.value = '';
+        }).catch(err => {
+            throw new Error("something went wrong")
+        })
+    } catch (err) {
+        input.style.border = "2px solid hsl(0, 87%, 67%)";
+        error.classList.remove('hidden');
+        error.textContent = "Please add a link";
+    }
 })
 
-const c2c =  function(){
-	history.addEventListener('click', function(e){
-		const copy = e.target.closest('#copy');
+const c2c = function() {
+    history.addEventListener('click', function(e) {
+        const copy = e.target.closest('#copy');
 
-		if(!copy) return;
+        if (!copy) return;
 
-		const cpy = copy.dataset.copyToClipboard;
-		console.log(cpy);
-		navigator.clipboard.writeText(cpy);
-		copy.style.backgroundColor = "hsl(255, 11%, 22%)";
-		copy.textContent = "Copied!";
-	})
+        const cpy = copy.dataset.copyToClipboard;
+        console.log(cpy);
+        navigator.clipboard.writeText(cpy);
+        copy.style.backgroundColor = "hsl(255, 11%, 22%)";
+        copy.textContent = "Copied!";
+    })
 }
 c2c();
+
+//for clearing local storage
+// localStorage.clear('links');
+// localStorage.clear('ol');
